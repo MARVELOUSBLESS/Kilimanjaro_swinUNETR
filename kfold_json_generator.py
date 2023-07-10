b2c4f4b719e802e67e281e35e4974b44a4573fed
Number One Hundred Eighty Three Guest <guest183@gra-login3.graham.sharcnet>
2023-07-10 19:48:28 -0400

    restarted git to ignore  model.pt files as they are too large

---
+++ kfold_json_generator.py
@@ -0,0 +1,76 @@
+from glob import glob
+import json
+import random
+import  nibabel as nib
+import numpy as np
+
+
+def view_data_shape(path_2_img:str):
+    r''' Given path to a nifti file, this function will load the contents as numpy array and prints its shape. 
+    '''
+    img:np.array = nib.load(path_2_img).get_fdata()
+    print(img.shape)
+    
+    return 0
+
+def kfold_data_dict(data_dir:str, num_folds:int):
+    r''' Given a directory of images from BraTS challege, it will map the patient files and stores them in a dictionary
+    inputs:
+        - data_dir: path to the training data
+        - num_folds: number of k fold cross validation 
+
+    output: 
+        k_fold_dict: a dictionary holding the following keys:
+            fold
+            image
+            label
+            training
+    '''
+    patient_dir:list = glob(data_dir + "*/")
+    random.shuffle(patient_dir)
+    # print(patient_dir)
+
+    # Initialize the dictionary holding k fold cross validation
+    kfold_dict:dict = {"training":[]}
+    # figure out how many patients are in a fold
+    num_patient_per_fold:int = int(len(patient_dir) / num_folds)
+    # print(num_patient_per_fold)
+
+    for k in range(num_folds):
+        # print(type(k))
+        
+        for patient in patient_dir[k*num_patient_per_fold: (k+1)*num_patient_per_fold]:
+            # print(patient)
+            # print()
+            temp_dict = {'fold':k}
+            temp_dict["image"] = glob(patient+"/*t*")
+            temp_dict["label"] = glob(patient+"/*seg*")[0]
+            kfold_dict["training"].append(temp_dict)
+            # print(temp_dict)
+            # break
+        # break
+
+    return kfold_dict
+
+def main():
+
+    # check the shape of a single image or segmentation file. 
+    # for brats 2021 
+    # path_2_img = "/scratch/guest183/BraTS_2021_data/RSNA_ASNR_MICCAI_BraTS2021_TrainingData_16July2021/TrainingData/BraTS2021_00000/BraTS2021_00000_seg.nii.gz"
+    # for brats 2023 africa 
+    path_2_img = "/scratch/guest183/BraTS_Africa_data/ASNR-MICCAI-BraTS2023-GLI-Challenge-TrainingData/BraTS-GLI-00387-000/BraTS-GLI-00387-000-seg.nii.gz"
+    view_data_shape(path_2_img)
+
+    # generate json file containing the path to n fold cross validation data 
+    # # get the number of folds
+    # num_folds:int = 5
+    # # get the path to the patient image folders
+    # data_dir:str = "/scratch/guest183/BraTS_Africa_data/ASNR-MICCAI-BraTS2023-GLI-Challenge-TrainingData/"
+    # kfold_dict:dict = kfold_data_dict(data_dir, num_folds)
+    # # print(len(kfold_dict["training"]))
+    # with open("brats23_africa_folds.json", 'w') as outfile:
+    #     json.dump(kfold_dict, outfile, indent=4)
+
+# DO NOT DELETE
+if __name__ == "__main__":
+    main()
---
+++ quality_assurance.py
@@ -0,0 +1,123 @@
+# libraries to load nifti images and save them
+import os
+import  nibabel as nib
+import numpy as np
+import matplotlib
+from glob import glob
+
+# libararies to load the model:
+import torch
+from monai.networks.nets import SwinUNETR
+
+# libraries to call another python scritp 
+import subprocess
+import sys
+
+
+
+
+def _test_save_image_png():
+    # to test save_image_png
+    path_2_img = "/scratch/guest183/BraTS_Africa_data/ASNR-MICCAI-BraTS2023-GLI-Challenge-TrainingData/BraTS-GLI-00387-000/BraTS-GLI-00387-000-seg.nii.gz"
+    outname = './qa_output/'+os.path.basename(path_2_img).split(".")[0]+".png"
+    # print(outname)
+    image=load_1_nifti(path_2_img)
+    save_image_png(image, 45, outname)
+
+
+def save_image_png(img:np.array,slice_number:int,outname:str):
+    r'''given a 3D numpy array, it will save a slice as a png image'''
+    # Access the image data and display it
+    slice = img[:, :,img.shape[2] // 2]
+    slice = img[:, :,slice_number]
+    matplotlib.image.imsave(outname, slice)
+    # matplotlib.image.imsave('./qa_output/'+os.path.basename(path_2_img).split(".")[0]+".png", slice)
+
+def load_1_nifti(path_2_img):
+    # Load the image using nibabel
+    return nib.load(path_2_img).get_fdata()
+
+def load_model(path_2_model:str, model_shape:dict):
+    r""" Given the path to a Swin UNETR model that has been previously trained, this function generates a new model 
+        and transfers the state of the trained model to the new model. It then returns the new model.
+
+    inputs:
+        - path_2_model := path to the .pt file
+        - model_shape := a dictionary containing the following keys and values
+            - "in_channels":int := # input channels
+            - "out_channels":int := # output channels
+            - "feature_size":int := the size of the features. it must be multiple of 12
+            - "use_checkopint":bool := to see if they want to use checkpoint or not
+    """
+    # initialize an empty model
+    model = SwinUNETR(
+        img_size=128,
+        in_channels=model_shape.in_channels,
+        out_channels=model_shape.out_channels,
+        feature_size=model_shape.feature_size,
+        drop_rate=0.0,
+        attn_drop_rate=0.0,
+        dropout_path_rate=0.0,
+        use_checkpoint=model_shape.use_checkpoint,
+    )
+    # load the state dictionary of the trained model
+    model_dict = torch.load(path_2_model)["state_dict"]
+    # fill up the new model with the loaded state dictionary
+    model.load_state_dict(model_dict)
+    # put the new model into evaluation mode
+    model.eval()
+
+    return model
+
+def _test_load_patient_to_tensor():
+    patient_dir = "/scratch/guest183/BraTS_Africa_data/ASNR-MICCAI-BraTS2023-GLI-Challenge-TrainingData/BraTS-GLI-00387-000/"
+    load_patient_to_tensor(patient_dir)
+
+def load_patient_to_tensor(path_2_patient:str):
+    r''' Given the folder of a patient, it will load the patient MRI images along with the segmentation into a dictionary of two tensors. 
+            The first tensor as the patient scan with the 4 channels the second tensor has the patient label. 
+    
+    inputs:
+        - path_2_patient := the global path to a single patient folder containing the following nifti files:
+            - t2w.nii.gz
+            - t2f.nii.gz
+            - t1n.nii.gz
+            - t1c.nii.gz
+    returns:
+        - patient_dict_tensor := a dictionary containing the following keys and values
+            - "input":torch.Tensor :=  a tensor of shape [4, 128, 128, 128] containig the patient mri scans in each channel.  
+                    the channels represent the t2w, t2f, t1n, and t1c scans.
+            - "label":torch.Tensor := a tensor of shape [3, 128, 128, 128] the ground truth segmentation of the patient tumors.  
+                    the channels represent the tumor core, enhancing tumor and tumor volume. 
+    '''
+
+    # let's get the global paths to nifti images
+    path_2_nifties = glob(path_2_patient+'*.nii.gz')
+    # print(path_2_nifties)
+
+    # load each nifti image
+    patient_tensors = dict((os.path.basename(img_name).split(".")[0], torch.from_numpy(load_1_nifti(img_name))) for img_name in path_2_nifties)
+    print(patient_tensors.keys())
+
+    
+
+def main():
+    # _test_save_image_png()      # test passed
+    _test_load_patient_to_tensor()
+
+    path_patient="/scratch/guest183/BraTS_Africa_data/ASNR-MICCAI-BraTS2023-GLI-Challenge-TrainingData/BraTS-GLI-00387-000/"
+    path_model='/home/guest183/research-contributions/SwinUNETR/BRATS21/runs/4_gpu_4_epochs/model_final.pt'
+
+        
+    # give the patient path to dataloader, then test the model on the loaded patient data
+
+
+
+ # DO NOT DELETE
+if __name__ == "__main__":
+
+    main()
+
+   
+    
+  
