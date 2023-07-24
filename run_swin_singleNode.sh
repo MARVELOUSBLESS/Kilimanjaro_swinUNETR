c5aaa5146274632980f7b5be86f7910937be866e
hosseinjafar <hosseinj.2555@gmail.com>
2023-07-24 19:02:43 -0400

    trying ot the v100 nodes

--- run_swin_singleNode.sh
+++ run_swin_singleNode.sh
@@ -2,10 +2,11 @@
 #SBATCH --account=def-training-wa
 # SBATCH --reservation hackathon-wr-gpu
 #SBATCH --nodes=1
-#SBATCH --gpus-per-node=t4:4
+#SBATCH --gpus-per-node=v100:8
+#SBATCH --constraint=cascade,v100
 #SBATCH --cpus-per-task=8
 #SBATCH --mem=64G
-#SBATCH --time=10:00:00
+#SBATCH --time=12:00:00
 module load python/3.9
 source /home/guest183/run_swinUNETR_kilimanjaro/SWIN_ENV/bin/activate
 # to solve the monai resolve_mode error 
@@ -25,9 +26,10 @@ path_data='/scratch/guest183/BraTS_Africa_data/'
 # code to run the main method of the SWIN UNER network
 # echo $path_swin'main.py'
 # to train on multiple GPU
-python $path_swin'main.py' --cache_dataset --save_checkpoint --roi_x=128 --roi_y=128 --roi_z=128 --distributed --lrschedule='cosine_anneal' --json_list=$path_swin'jsons/brats23_africa_folds.json' --sw_batch_size=4 --batch_size=1 --data_dir=$path_data --val_every=10 --infer_overlap=0.7 --in_channels=4 --spatial_dims=3 --save_checkpoint --use_checkpoint --feature_size=48 --max_epochs=200 --logdir='rio_128_epoch_200_gpu_v100_32GB'
+python $path_swin'main.py' --fold=0 --smartcache_dataset --roi_x=128 --roi_y=128 --roi_z=128 --distributed --lrschedule='cosine_anneal' --json_list=$path_swin'jsons/brats23_africa_folds.json' --sw_batch_size=4 --batch_size=1 --data_dir=$path_data --val_every=20 --infer_overlap=0.7 --in_channels=4 --spatial_dims=3 --save_checkpoint --use_checkpoint --feature_size=48 --max_epochs=200 --logdir='rio_128_epoch_200_gpu_v100_32GB'
 
-# --pretrained_dir=$path_swin'runs/4_gpu_60_epochs/' --pretrained_model_name='model_final.pt'
+# --resume_ckpt --pretrained_dir=$path_swin'runs/4_gpu_60_epochs/' --pretrained_model_name='model_final.pt'
+# 
 
 # --roi_x=128 --roi_y=128 --roi_z=128
 
