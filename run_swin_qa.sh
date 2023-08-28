c88e8a4be8599748c0f5976ffbd82b30e47cb34c
hosseinjafar <hosseinj.2555@gmail.com>
2023-08-28 15:30:25 -0400

    nothing important

--- run_swin_qa.sh
+++ run_swin_qa.sh
@@ -17,12 +17,12 @@ path_data='/home/odcus/Data/BraTS_Africa_data/'
 export PYTHONPATH=$path_swin
 
 
-# generate model predictions 
-# python $path_swin'test.py'  --infer_overlap=0.7\
-#  --data_dir=$path_data --exp_name='epoch100_baseModel_GLI_test'\
-#  --json_list=$path_swin'jsons/brats23_gli_test.json'\
-#  --pretrained_dir=$path_swin'pretrained_models/'\
-#  --pretrained_model_name='model-epoch100-baseModel-2023.pt'
+# # generate model predictions 
+python $path_swin'test.py'  --infer_overlap=0.7\
+ --data_dir=$path_data --exp_name='epoch100_baseModel_GLI_test'\
+ --json_list=$path_swin'jsons/brats23_gli_test.json'\
+ --pretrained_dir=$path_swin'pretrained_models/'\
+ --pretrained_model_name='model-epoch100-baseModel-2023.pt'
 
 # run quality assurance on model predictions
-python $path_swin'kilimajaro_scripts/quality_assurance.py'
+# python $path_swin'kilimajaro_scripts/quality_assurance.py'
